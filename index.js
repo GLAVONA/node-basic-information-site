fda3bb7178a9bfa2865d292f9f928ec8c3f758ad
GLAVONA <97589269+GLAVONA@users.noreply.github.com>
2023-02-17 18:57:41 +0200

    redone with express

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
-const http = require("http");
-const fs = require("fs");
+const express = require('express')
+const app = express();
+const {readFile}= require("fs").promises;
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    fs.readFile("index.html", (err, data) => {
-      if (err) throw err;
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
-  } else if (req.url === "/about") {
-    fs.readFile("about.html", (err, data) => {
-      if (err) throw err;
-      res.end(data);
-    });
-  } else if (req.url === "/contact-me") {
-    fs.readFile("contact-me.html", (err, data) => {
-      if (err) throw err;
-      res.end(data);
-    });
-  }
-  else{
-    fs.readFile("404.html", (err,data)=>{
-        if (err) throw err;
-        res.end(data);
-    })
-  }
-});
+app.get("/",async(req,res)=>{
+  res.send(await readFile('./index.html','utf-8'))
+})
 
-server.listen(5050, () => {
-  console.log("listening");
-});
+app.get("/about",async(req,res)=>{
+  res.send(await readFile('./about.html','utf-8'))
+})
+
+app.get("/contact-me",async(req,res)=>{
+  res.send(await readFile('./contact-me.html','utf-8'))
+})
+
+app.use(async(req,res)=>{
+  res.status(404).send(await readFile('./404.html','utf-8'))
+})
+
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/") {
+//     fs.readFile("index.html", (err, data) => {
+//       if (err) throw err;
+//       res.writeHead(200, { "Content-Type": "text/html" });
+//       res.end(data);
+//     });
+//   } else if (req.url === "/about") {
+//     fs.readFile("about.html", (err, data) => {
+//       if (err) throw err;
+//       res.end(data);
+//     });
+//   } else if (req.url === "/contact-me") {
+//     fs.readFile("contact-me.html", (err, data) => {
+//       if (err) throw err;
+//       res.end(data);
+//     });
+//   }
+//   else{
+//     fs.readFile("404.html", (err,data)=>{
+//         if (err) throw err;
+//         res.end(data);
+//     })
+//   }
+// });
+
+
+app.listen(5050,()=>{console.log("listening")})
